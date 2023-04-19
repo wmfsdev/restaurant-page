38556768ffc7cdc51f3493fd720d550de1e000e1
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-04-19 19:33:22 +0100

    added HtmlWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
   entry: {
     index: './src/index.js',
     home: './src/home.js',
-    menu: './src/menu.js'
+    menu: './src/menu.js',
+    contact: './src/contact.js',
   },
 
-  // plugins: [
+  plugins: [
 
-  //   new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({
 
-  //     title: 'Restaurant Page',
+      title: 'Restaurant Page',
 
-  //   }),
+    }),
 
-  // ],
+  ],
 
   output: {
     filename: '[name].bundle.js',
